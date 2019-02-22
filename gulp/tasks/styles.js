a1c112fc4200b7e4ce4d2f925df3b5f2e6f0874e
Filip <filipkazmierczak18@gmail.com>
2019-02-22 10:25:34 +0100

    Mała zmiana

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,11 +5,12 @@ const simpleVars = require('postcss-simple-vars');
 const nested = require('postcss-nested');
 const cssImport = require('postcss-import');
 const mixins = require('postcss-mixins');
+const hexrgba = require('postcss-hexrgba');
 
 
 gulp.task("styles", function () {
  return gulp.src('./app/assets/styles/styles.css')
- .pipe(postcss([cssImport, mixins, autoprefixer, simpleVars, nested]))
+ .pipe(postcss([cssImport, mixins, simpleVars, nested, hexrgba, autoprefixer]))
  .on('error', function(errorInfo) {
   console.log(errorInfo.toString())
   this.emit('end');
