1db354f218b7888853dc4a5c00300a151c43c402
Filip <filipkazmierczak18@gmail.com>
2019-02-27 09:25:09 +0100

    Completed the reveal on scroll behavior.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,38 +1,4 @@
-// class Person{
-//   constructor(name, age){
-//     this.name = name,
-//     this.age = age;
-//   }
-//   callName(){
-//     console.log(`Mam na imię ${this.name} i mam ${this.age} lat`)
-//   }  
-//  };
-
-// const names = [{
-//   name: "Filip",
-//   age: 25,
-
-//  },
-//  {
-//   name: "Izaaddadd",
-//   age: 24
-//  }
-//  ]
-// function callAllNames(){
-//   names.map(person => new Person(person.name, person.age).callName())
-// }
-
-// callAllNames()
-
-
-
-// $('.site-header__menu-icon').click(function(){
-//   alert("Działa")
-// })
-
-
-
-
+//MobileMenu: //
 class MobileMenu {
   constructor() {
     this.siteHeader = $(".site-header")
@@ -40,16 +6,67 @@ class MobileMenu {
     this.menuContent = $('.site-header__menu-content');
     this.events();
   }
-  events(){
+  events() {
     this.menuIcon.click(() => this.toggleTheMenu());
     console.log(this)
   }
   toggleTheMenu() {
-     this.menuContent.toggleClass("site-header__menu-content--is-visible");
-     console.log(this);
-     this.siteHeader.toggleClass("site-header--is-expanded");
-     this.menuIcon.toggleClass('site-header__menu-icon--close-x')
-   }
+    this.menuContent.toggleClass("site-header__menu-content--is-visible");
+    console.log(this);
+    this.siteHeader.toggleClass("site-header--is-expanded");
+    this.menuIcon.toggleClass('site-header__menu-icon--close-x')
   }
+}
 
 const test = new MobileMenu();
+
+
+//Reveal on Scroll: //
+class RevealOnScroll {
+  constructor(classElement, animationDelay) {
+    this.itemsToReveal = classElement;
+    this.animationDelay = animationDelay;
+    this.hideInitially();
+    this.createOnScroll();
+  }
+  hideInitially() {
+    this.itemsToReveal.addClass('reveal-item');
+  }
+
+  createOnScroll() {
+    // this.itemsToReveal.each(function(){
+    //   const currentItem = this;
+    //   $(document).scroll(function(){ 
+    //     const scrollTop = $(document).scrollTop();
+    //     const offsetTop = $(currentItem).offset().top;
+    //     const height = $(currentItem).outerHeight();
+    //     console.log(scrollTop, offsetTop);
+    //     if(scrollTop >= offsetTop - height){
+    //       $(currentItem).addClass('reveal-item--is-visible');
+    //     } else if(scrollTop < offsetTop - height){
+    //       $(currentItem).removeClass('reveal-item--is-visible');
+    //     }
+    //   })    
+    // })
+    const that = this;
+    [...this.itemsToReveal].forEach(element => {
+      document.addEventListener('scroll', function () {
+        const scrollTop = window.scrollY;
+        const offsetTop = element.offsetTop;
+        const height = element.offsetHeight;
+        console.log(scrollTop,  offsetTop - height);
+        if (scrollTop >= offsetTop - height / that.animationDelay) {
+          element.classList.add('reveal-item--is-visible');
+        } else if (scrollTop < offsetTop - height / that.animationDelay) {
+          element.classList.remove('reveal-item--is-visible');
+        }
+      })
+    })
+
+  }
+
+
+}
+
+new RevealOnScroll($('.feature-item'), 1);
+new RevealOnScroll($('.testimonial'), 1)
